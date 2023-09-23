fdd9cf32bf717c8b817cbc04d0c5e5bca3b848ce
undavallisridevi <86953548+undavallisridevi@users.noreply.github.com>
2023-09-23 20:14:12 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Portfolio
-Created with CodeSandbox
+# My-Portfolio
---
+++ script.js
@@ -0,0 +1,44 @@
+"use strict";
+
+/**
+ * add event listener on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+};
+
+/**
+ * SCROLL REVEAL
+ */
+
+const revealElements = document.querySelectorAll("[data-reveal]");
+const revealDelayElements = document.querySelectorAll("[data-reveal-delay]");
+
+const reveal = function () {
+  for (let i = 0, len = revealElements.length; i < len; i++) {
+    if (
+      revealElements[i].getBoundingClientRect().top <
+      window.innerHeight / 1.2
+    ) {
+      revealElements[i].classList.add("revealed");
+    }
+  }
+};
+
+for (let i = 0, len = revealDelayElements.length; i < len; i++) {
+  revealDelayElements[i].style.transitionDelay =
+    revealDelayElements[i].dataset.revealDelay;
+}
+
+window.addEventListener("scroll", reveal);
+window.addEventListener("load", reveal);
+
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
